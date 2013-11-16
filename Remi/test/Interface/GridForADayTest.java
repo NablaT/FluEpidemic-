22cb02e54a1a7dc89e0dacec54b96259d883fcd7
Tornado089 <tornado089@TornadoPC.(none)>
2013-11-16 14:25:45 +0100

    Tests de PieceOfDisplay & GridForADay

--- Remi/test/Interface/GridForADayTest.java
+++ Remi/test/Interface/GridForADayTest.java
@@ -6,12 +6,42 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import Environment.Case;
 import Environment.GameRound;
+import Environment.Grid;
+import Living.Human;
+import Living.Pig;
 
 public class GridForADayTest {
 
+	public GridForADay gfad;
+	public GameRound gr;
+	
 	@Before
 	public void setUp() throws Exception {
+		
+		// Creation des cases
+		Case cases[][] = new Case[5][5];
+		for(int i = 0; i<5; i++){
+			for(int j = 0; j<5; j++){
+				cases[i][j] = new Case(i, j);
+			}
+		}
+		
+		// Remplissage de cases
+		cases[1][2].setLB(new Human());
+		cases[3][3].setLB(new Pig());
+		
+		// Creation de la grille
+		Grid g = new Grid(10, 10);
+		g.setGrid(cases, 5, 5);
+		
+		// Creation du gameRound
+		this.gr = new GameRound(5, 5);
+		this.gr.setGrid(g);
+		
+		// Creation de la grille
+		this.gfad = new GridForADay(gr);
 	}
 
 	@After
@@ -20,22 +50,71 @@ public class GridForADayTest {
 
 	@Test
 	public void testGridForADay() {
+		assertEquals(gr, gfad.game);
 	}
 
 	@Test
 	public void testBuildTheGrid() {
-	}
+		
+		// Recuperation de l'affichage
+		String display = gfad.getDisplay();
+		
+		// Declaration de la ligne courante
+		char[] ligneCourante = new char[80];
 
-	@Test
-	public void testBorder() {
-	}
+		// Taille d'une ligne
+		int tailleLigne = 46; 
+		
+		// Declaration de la ligne de date
+		String date = "Today : MONDAY";
+		char[] dateChar = new char[80];
+		date.getChars(0, 14, dateChar, 0);
+		
+		// Declaration d'une ligne de bord
+		String bord = "+--------+--------+--------+--------+--------+";
+		char[] bordChar = new char[80];
+		bord.getChars(0, tailleLigne, bordChar, 0);
+		
+		// Declaration d'une ligne vide
+		String vide = "|        |        |        |        |        |";
+		char[] videChar = new char[80];
+		vide.getChars(0, tailleLigne, videChar, 0);
 
-	@Test
-	public void testContent() {
-	}
+		// Declaration d'une avec humains
+		String humains = "|        |   H H  |        |        |        |";
+		char[] humainsChar = new char[80];
+		humains.getChars(0, tailleLigne, humainsChar, 0);
+		
+		// Declaration d'une avec cochons
+		String cochons = "|        |        |        |   P H  |        |";
+		char[] cochonsChar = new char[80];
+		cochons.getChars(0, tailleLigne, cochonsChar, 0);
+		
+		// Test des lignes
+		display.getChars(0, 14, ligneCourante, 0);
+		assertEquals(String.valueOf(dateChar), String.valueOf(ligneCourante));
 
-	@Test
-	public void testGetDisplay() {
-	}
+		int index = 15;
 
+		//assertEquals(gfad.getDisplay(), "");
+		
+		for(int i = 0; i < 5; i++){
+			
+			display.getChars(index, index + tailleLigne, ligneCourante, 0);
+			assertEquals(String.valueOf(bordChar), String.valueOf(ligneCourante));
+			index += tailleLigne + 1;
+			
+			
+			display.getChars(index, index + tailleLigne, ligneCourante, 0);
+			if(i == 2){assertEquals(String.valueOf(humainsChar), String.valueOf(ligneCourante));}
+			else if(i == 3){assertEquals(String.valueOf(cochonsChar), String.valueOf(ligneCourante));}
+			else{assertEquals(String.valueOf(videChar), String.valueOf(ligneCourante));}
+			index += tailleLigne + 1;
+			
+			display.getChars(index, index + tailleLigne, ligneCourante, 0);
+			assertEquals(String.valueOf(bordChar), String.valueOf(ligneCourante));
+			index += tailleLigne + 1;
+		}
+	
+	}
 }
--- Remi/test/Interface/PieceOfDisplayTest.java
+++ Remi/test/Interface/PieceOfDisplayTest.java
@@ -3,13 +3,20 @@ package Interface;
 import static org.junit.Assert.*;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import Environment.JourEnum;
 
 public class PieceOfDisplayTest {
 
+	public PieceOfDisplay pod;
+	
 	@Before
 	public void setUp() throws Exception {
+		this.pod = new PieceOfDisplay();
 	}
 
 	@After
@@ -18,12 +25,14 @@ public class PieceOfDisplayTest {
 
 	@Test
 	public void testGetDay() {
-		fail("Not yet implemented");
+		pod.day = "testDay";
+		assertEquals("testDay", pod.getDay());
 	}
 
 	@Test
 	public void testSetDay() {
-		fail("Not yet implemented");
+		pod.setDay(JourEnum.TUESDAY);
+		assertEquals("Today : TUESDAY", pod.day);
 	}
 
 }
